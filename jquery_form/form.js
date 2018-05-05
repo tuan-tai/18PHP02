b2c83c7eba5cf55849603b30bca4b6573c129fed
tuan-tai <tuantai568@gmail.com>
2018-05-06 05:17:15 +0700

    Jquery

--- jquery_form/form.js
+++ jquery_form/form.js
@@ -1,6 +1,11 @@
 $('#submitInfo').click(function() {
 	if ($("#name").val() != '' && $("#age").val() != '') {
+
+		$("input[name*='hobbies']").prop("checked", false);
 		$("#hobbies").show();
+		$("#result").html($("#name").val());
+		$("#hobbies_result").html('<h3>HOBBIES</h3>');
+
 		if ($("input[name*='gender']:radio:checked").val() == 'male') {
 			$("#hobbies #male").show();
 			$("#hobbies #female").hide();
@@ -8,7 +13,7 @@ $('#submitInfo').click(function() {
 				src: "male.jpeg", 
 				alt: "Hinh anh nam"
 			});
-			$("#result").css("color", "red").html($("#name").val());
+			$("#avatar .container div").css("color", "red");
 		}
 		else {
 			$("#hobbies #male").hide();
@@ -18,21 +23,32 @@ $('#submitInfo').click(function() {
 				alt: "Hinh anh nu"
 			});
 			if ($("#age").val() < 1997) {
-				$("#result").css("color", "blue").html($("#name").val());
+				$("#avatar .container div").css("color", "blue");
 			}
 			else {
-				$("#result").css("color", "yellow").html($("#name").val());
+				$("#avatar .container div").css("color", "yellow");
 			}
 		}
 	}
 });
 
 $("#submitHobbies").click(function() {
+	
+	if ($("input[name*='gender']:radio:checked").val() == 'male') {
+		$("#avatar .container div").css("color", "red");
+	}
+	else {
+		if ($("#age").val() < 1997) {
+			$("#avatar .container div").css("color", "blue");
+		}
+		else {
+			$("#avatar .container div").css("color", "yellow");
+		}
+	}
+
 	var hb = '<h3>HOBBIES</h3>';
-	// $("#hobbies_result").html($("input[name*='hobbies']:checked").each().val());
 	$("input[name*='hobbies']:checked").each(function () {
 		hb = hb + '<p>' + $(this).val() + '</p>';
 	});
 	$("#hobbies_result").html(hb);
-	$("input[name*='hobbies']").prop("checked", false);
 });
