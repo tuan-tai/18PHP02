46e315207a45a8a97dc1547775560e9931a73dde
Tuan Tai <tuantai@Tuans-MacBook-Pro.local>
2018-06-28 09:12:30 +0700

    update

--- session-24/products/add_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start(); 
-	if(isset($_SESSION['userInfo']) && $_SESSION['userInfo']['role'] == 1){
-		echo "Dang san pham tai day!";	
-	}else{
-		echo "Ban khong co quyen dang san pham";	
-	}
-	
-?>
