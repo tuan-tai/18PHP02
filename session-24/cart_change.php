47ad3784d159b9c5c8499ad4dfe5f5117c238dbd
Tuan Tai <tuantai@Tuans-MacBook-Pro.local>
2018-07-02 19:32:10 +0700

    update

--- session-24/cart_change.php
+++ session-24/cart_change.php
@@ -2,7 +2,9 @@
 	session_start();
 	if (isset($_GET['action']) && isset($_GET['id'])) {
 		if ($_GET['action'] == '-') {
-			if ($_SESSION['cart'][$_GET['id'] > 1]) {
+			if ($_SESSION['cart'][$_GET['id']] == 1) {
+				header("Location: cart_show.php");
+			} else {
 				$_SESSION['cart'][$_GET['id']] = $_SESSION['cart'][$_GET['id']] - 1;
 			}
 		} else {
---
+++ session-24/layouts/promotion.php
@@ -0,0 +1,30 @@
+<div class="promo-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-sm-6">
+                <div class="single-promo bg--blue">
+                    <i class="fa fa-refresh"></i>
+                    <p>30 Days return</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6">
+                <div class="single-promo bg--yellow">
+                    <i class="fa fa-truck"></i>
+                    <p>Free shipping</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6">
+                <div class="single-promo bg--red">
+                    <i class="fa fa-lock"></i>
+                    <p>Secure payments</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6">
+                <div class="single-promo bg--green">
+                    <i class="fa fa-gift"></i>
+                    <p>New products</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
